faccb67d8c4d4c20b9d8627ebb8bd3a66fe76db3
11Alexei11 <lesha.borovik@gmail.com>
2023-06-10 18:40:06 +0200

    were added urls

--- app/car_price_predict_site/entering_data/urls.py
+++ app/car_price_predict_site/entering_data/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("prediction-form/", views.prediction_form, name='prediction-form'),
+    path("calculate_price/", views.calculate_price, name="calculate_price")
 ]
