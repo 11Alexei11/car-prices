f8fce6dbda6bdf77dafde3878f4517a4b366dd9f
11Alexei11 <lesha.borovik@gmail.com>
2023-06-18 10:01:17 +0200

    was added register view and saving car info to db

--- app/car_price_predict_site/entering_data/views.py
+++ app/car_price_predict_site/entering_data/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, HttpResponse
 from django.template import loader
 import pickle as pkl
 import pandas as pd
-import json
 import os
+import uuid
+import datetime
 
-from .models import Car
+from .models import Car, User
 
 with open(f'{os.getcwd()}/../../models/full_pipeline_regressor.pkl', 'rb') as f:
     model = pkl.load(f)
@@ -18,11 +19,35 @@ def prediction_form(request):
     return render(request, 'entering_data/prediction_form.html')
 
 def calculate_price(request):
+    if not request.user.is_authenticated:
+        HttpResponse('You should registrate firstly to use service')
+
+    car_view = Car(request.POST)
+    car_view.ID = uuid.uuid4()
+    car_view.USER_ID = request.user.id
+
     form_dictionary = {key: request.POST.get(key, None) for key in Car.ColumnNames.column_names() \
                        if key not in ('id', 'user_id', 'price') and not key.startswith('__')}
 
     x = pd.DataFrame({key: [value] for key, value in form_dictionary.items()})
-
     predictions = model.predict(x)[0]
 
+    car_view.PRICE = predictions
+
     return HttpResponse(f"{predictions}")
+
+
+def register_view(request):
+    if request.POST:
+        name = request.POST['name']
+        surname = request.POST['surname']
+        email = request.POST['email']
+
+        if not User.objects.filter(name=name, surname=surname, email=email).exists():
+            user = User(
+                name=name,
+                surname=surname,
+                email=email,
+                registration_date=datetime.datetime.now()
+            )
+            user.save()
