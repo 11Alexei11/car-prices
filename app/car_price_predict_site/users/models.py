60689ee4bf8f191783570c66ebf3e454fa33a490
11Alexei11 <lesha.borovik@gmail.com>
2023-07-23 19:31:08 +0200

    was added User model and corresponding view

--- app/car_price_predict_site/users/models.py
+++ app/car_price_predict_site/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    ID = models.BigAutoField(primary_key=True)
+    NAME = models.CharField(max_length=30)
+    SURNAME = models.CharField(max_length=30)
+    EMAIL = models.CharField(max_length=30)
+    REGISTRATION_DATE = models.DateTimeField()
--- app/car_price_predict_site/users/views.py
+++ app/car_price_predict_site/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import User
+
+import datetime
 
 # Create your views here.
+def register_view(request):
+    if request.POST:
+        name = request.POST['name']
+        surname = request.POST['surname']
+        email = request.POST['email']
+
+        if not User.objects.filter(name=name, surname=surname, email=email).exists():
+            user = User(
+                name=name,
+                surname=surname,
+                email=email,
+                registration_date=datetime.datetime.now()
+            )
+            user.save()
