e57de01bb290f39ff7a1b594465f415f70c9ad6a
11Alexei11 <lesha.borovik@gmail.com>
2023-07-25 08:13:49 +0200

    was edited table in models

--- app/car_price_predict_site/users/models.py
+++ app/car_price_predict_site/users/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    ID = models.BigAutoField(primary_key=True)
+    ID = models.BigAutoField(primary_key=True, auto_created=True)
     NAME = models.CharField(max_length=30)
-    SURNAME = models.CharField(max_length=30)
     EMAIL = models.CharField(max_length=30)
-    REGISTRATION_DATE = models.DateTimeField()
+    REGISTRATION_DATE = models.DateTimeField(auto_now_add=True)
+    PASSWORD = models.CharField(max_length=255)
