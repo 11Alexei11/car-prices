3716ed98cdcf4cc004438b6607fb92ac4f4d1444
11Alexei11 <lesha.borovik@gmail.com>
2023-08-13 16:08:16 +0200

    was changed endpoint for calculation of price

--- app/car_price_predict_site/price_predictions/urls.py
+++ app/car_price_predict_site/price_predictions/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("prediction-form/", views.prediction_form, name='prediction-form'),
-    path("calculate_price/", views.calculate_price, name="calculate_price")
+    path("calc-price/", views.calculate_price, name="calc-price")
 ]
